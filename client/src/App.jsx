651cbe0de2e3fc3b404188cad6493968f7d90594
Mike Kinnear <mikekinnear06@hotmail.com>
2025-06-21 21:22:37 -0700

    Intial file structure with client/server folders

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './assets/underconstructionlogo.png'
+
+function App() {
+  return (
+    <>
+      <div className="flex">
+        <img className="size-24 rounded" src={logo} />
+      </div>
+    </>
+  )
+}
+
+export default App
