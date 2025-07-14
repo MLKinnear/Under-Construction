e4d407cca168f3b7ad9ffb88d262fb821a0d17b7
Mike Kinnear <mikekinnear06@hotmail.com>
2025-07-13 17:01:53 -0700

    Changed baseURL with error checking client side calls /api not localhost on deployment

--- client/src/utils/axiosConfig.js
+++ client/src/utils/axiosConfig.js
@@ -1,6 +1,10 @@
 import axios from 'axios'
 
-const baseURL = import.meta.env.VITE_API_URL || `${window.location.origin}/api`;
+const baseURL = import.meta.env.VITE_API_URL;
+
+if (!baseURL) {
+    throw new Error('VITE_API_URL is not defined!');
+}
 
 const instance = axios.create({ baseURL })
 
